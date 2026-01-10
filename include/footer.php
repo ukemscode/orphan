2c7af9bc951278a22c638d6fdf86707566ff2334
Ukeme Peter <ukemepeter48@gmail.com>
2026-01-10 15:35:18 +0100

    contact details changes

--- include/footer.php
+++ include/footer.php
@@ -1,51 +1,53 @@
 <footer>
-      <div>
-        <div>
-          <div class="head">
-            <img src="/orphan/assets/love-svgrepo-com.svg" alt="products logo" class="logo" />
-            <nav>Babys Home</nav>
-          </div>
-          <h6>
-            Providing love, care, and hope to children in need. Together, we can
-            make a difference in their lives.
-          </h6>
-        </div>
-
-        <div class="contactf" >
-          <nav>Contact Us</nav>
-          <ul>
-            <li>
-             <i class="bi bi-geo-alt-fill"></i>
-              <h6>23 Etim,Uyo LGA</h6>
-            </li>
-            <li>
-              <i class="bi bi-telephone"></i>
-              <h6>09060280870</h6>
-            </li>
-            <li>
-              <i class="bi bi-envelope"></i>
-              <h6>
-                <a href="mailto:babyhome@gmail.com"> babyhome@gmail.com</a>
-              </h6>
-            </li>
-          </ul>
-        </div>
-
-        <div class="fxc">
-          <nav>Follow Us</nav>
-          <div>
-            <div><i class="bi bi-facebook"></i></div>
-            <div><i class="bi bi-twitter-x"></i></div>
-            <div><i class="bi bi-instagram"></i></div>
-          </div>
-          <h6>
-            Stay connected for updates on our children and upcoming events.
-          </h6>
-        </div>
+  <div>
+    <div>
+      <div class="head">
+        <img src="/orphan/assets/love-svgrepo-com.svg" alt="products logo" class="logo" />
+        <nav>Babys Home</nav>
       </div>
-
       <h6>
-        &copy; 2025 Hope Haven Orphanage. All rights reserved. Built with love
-        and care.
+        Providing love, care, and hope to children in need. Together, we can
+        make a difference in their lives.
       </h6>
-    </footer>
+    </div>
+
+    <div class="contactf">
+      <nav>Contact Us</nav>
+      <ul>
+        <li>
+          <i class="bi bi-geo-alt-fill"></i>
+          <h6>Omodu Street, Opp Bobby Filling Station, Mgbuoba, Off NTA, Rumuokwuta Rd, Port Harcourt 500272</h6>
+        </li>
+        <li>
+          <i class="bi bi-telephone"></i>
+          <h6>08096121842</h6>
+        </li>
+        <li>
+          <i class="bi bi-envelope"></i>
+          <h6>
+            <a href="mailto:info@rhemaorphanagehome.org.ng">
+
+              info@rhemaorphanagehome.org.ng</a>
+          </h6>
+        </li>
+      </ul>
+    </div>
+
+    <div class="fxc">
+      <nav>Follow Us</nav>
+      <div>
+        <div><i class="bi bi-facebook"></i></div>
+        <div><i class="bi bi-twitter-x"></i></div>
+        <div><i class="bi bi-instagram"></i></div>
+      </div>
+      <h6>
+        Stay connected for updates on our children and upcoming events.
+      </h6>
+    </div>
+  </div>
+
+  <h6>
+    &copy; 2025 Hope Haven Orphanage. All rights reserved. Built with love
+    and care.
+  </h6>
+</footer>
